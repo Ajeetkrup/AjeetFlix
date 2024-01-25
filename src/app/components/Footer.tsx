5f0d60431e76a7eb81daa2aa2b9560c6ccea0bba
Ajeetkrup <ajeetkrup401@gmail.com>
2024-01-25 23:42:20 +0530

    footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="min-h-12 bg-indigo-400 w-full flex justify-center py-3 text-sm font-normal text-slate-50 fixed right-0 bottom-0 left-0">
-      Made with ❤️ by Ajeet Upadhyay
+      Made with ❤️ by Ajeet
     </div>
   );
 };
